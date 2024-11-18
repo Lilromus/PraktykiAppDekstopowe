c8319796ca2f6f200bf969fb6a7582714424b194
roman <roman@DESKTOP-G3GKTTM>
2024-11-18 18:04:26 +0200

    zad 3

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameInput.Text;
+            string password = PasswordInput.Password;
+
+           
+            if (username == "Admin" && password == "Owca")
+            {
+                
+                CalculatorWindow calculatorWindow = new CalculatorWindow();
+                calculatorWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                
+                ErrorText.Text = "Niepoprawna nazwa użytkownika lub hasło.";
+            }
+        }
+    }
+}
