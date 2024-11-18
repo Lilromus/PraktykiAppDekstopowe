c6b2688e2ca50e68d6069f1b368b5629542fbb53
roman <roman@DESKTOP-G3GKTTM>
2024-11-18 17:09:49 +0200

    zad 2

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 
 namespace WpfApp1
 {
     public partial class MainWindow : Window
     {
-
-        private List<decimal> bruttoList = new List<decimal>();
-        private List<decimal> nettoList = new List<decimal>();
-
         public MainWindow()
         {
             InitializeComponent();
@@ -17,28 +11,33 @@ namespace WpfApp1
 
         private void CalculateNetto_Click(object sender, RoutedEventArgs e)
         {
-            try
+            
+            if (decimal.TryParse(BruttoInput.Text, out decimal brutto))
             {
+               
+                decimal stawka = Oblicz_Stawke();
 
-                decimal brutto = decimal.Parse(BruttoInput.Text);
+               
+                decimal netto = brutto * (1 - stawka);
 
-                decimal netto = brutto * 0.88m;
-
-
-                bruttoList.Add(brutto);
-                nettoList.Add(netto);
-
-
+             
                 BruttoList.Items.Add($"Brutto: {brutto}zł");
                 NettoList.Items.Add($"Netto: {netto}zł");
-
-
-                BruttoInput.Clear();
             }
-            catch (FormatException)
+            else
             {
-                MessageBox.Show("Wprowadź poprawną liczbę dla pensji brutto.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Wprowadź poprawną wartość dla pensji brutto.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+        }
+
+       
+        private decimal Oblicz_Stawke()
+        {
+            if (stawk8_5.IsChecked == true)
+                return 0.085m; 
+            if (stawk19.IsChecked == true)
+                return 0.19m; 
+            return 0.12m; 
         }
     }
 }
